6a57b4bc5969195ae289df8cc26c735e3649205a
Aaron Saray <me@aaronsaray.com>
2016-06-30 13:29:55 -0500

    removed notion of premium content indicator from options.

--- templates/admin/help.tpl.php
+++ templates/admin/help.tpl.php
@@ -4,6 +4,4 @@
 <dl>
 <dt><?= t('Paywall and Wallet') ?></dt>
 <dd><?= t('Add the paywall and embedded wallet to your site.  The paywall can be configured to be server-side or embedded (client side).') ?></dd>
-<dt><?= t('Premium Content Indicator') ?></dt>
-<dd><?= t('Indicate premium content on your site by tagging it as such.  This helps users understand if a paywall action is about to occur.') ?></dd
 </dl>
